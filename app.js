b7a9573a0ea65665e69febbdda94aea75076d125
Sofia Björkholm <sofia.bjorkholm@hotmail.com>
2018-11-28 18:33:46 +0100

    final fix for moodresult

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const movies = require('./database/imdb');
 const request = require('request');
 //import weather, color & rorschack functions
 const weatherFunctions = require('./moods/weather.js')
-const colorFunctions = require('.//moods/color.js')
+const colorFunctions = require('./moods/color.js')
 const rorschachFunctions = require('./moods/rorschach.js')
+
 //console.log(movies.database.showAllMovies(movies.database.movies)[0].genres);
 //console.log(movies.database.searchMovieByTitle('godfather')[0]);
 //console.log(movies.database.searchMovieByGenre('Action'));
@@ -31,79 +32,51 @@ app.get('/', (req, res) => {
   res.render('index', {weatherResult: false});
 });
 
-/*------------- WEATHER -----------------*/
-
-app.post('/weather', (req, res) => {
-  let cityInput = req.body.city
-    if(cityInput === 'Amsterdam') {
-request('https://api.darksky.net/forecast/dd99484e9987befc5af779e47abe4908/52.3680,4.9036?units=si', { 
-    json: true }, (err, respond, body) => {
-        const icon = `${body.currently.icon}` 
-        const humidity = `${body.currently.humidity}` 
-        const temperature = `${body.currently.temperature}`
-        let weatherResult = weatherFunctions.weather(icon, humidity, temperature);
-        console.log(weatherResult)
-        res.send({weatherResult: weatherResult})
-      });
-    }
-})
-
-/*---------------- COLOR -------------------*/
-
-
-app.post('/color', (req, res) => {
-  let color = req.body.color // '0'
-  let colorResult = colorFunctions.color(color);
- //console.log(typeof color)
-
-res.send({colorResult: colorResult})
-})
-
-/*-------------- RORSCHACH ----------------*/
-
-app.post('/rorschack', (req, res) => {
-  let rorschach = req.body.rorschach  //how to take value from ejs?
-  let rorschachResult = rorschachFunctions.rorschach(rorschach)
-res.send({rorschachResult: rorschachResult})
-})
 
 /*------------ RESULT REQUEST ------------*/
 app.post('/results', (req, res) => {
-let rorschachResult = rorschachFunctions.rorschach()
-let colorResult = colorFunctions.color();
-let weatherResult = weatherFunctions.weather()
-
-let result = [weatherResult, colorResult, rorschachResult] 
-  function getSum(x, y) { 
-        return x + y;
+  console.log(req.body)
+  let cityInput = req.body.city
+  let weatherResult = weatherFunctions.weather();
+  if(cityInput === 'Amsterdam') {
+request('https://api.darksky.net/forecast/dd99484e9987befc5af779e47abe4908/52.3680,4.9036?units=si', { 
+  json: true }, (err, respond, body) => {
+      const icon = `${body.currently.icon}` 
+      const humidity = `${body.currently.humidity}` 
+      const temperature = `${body.currently.temperature}`
+      weatherResult = weatherFunctions.weather(icon, humidity, temperature);
+      console.log(icon)
+    });
   }
-let sum = (result.reduce(getSum)) 
-  function average(x){ 
-    return Math.floor(sum/x.length) //math.ceil always rounds up 
-  }
-let Average = average(result)
-let total = Average.toString()
-console.log(total)
-const response = movies.database.randomMovie(total);
-console.log(response)
-let list = []
-let movie1 = movies.database.randomMovie(total);
-let movie2 = movies.database.randomMovie(total);
-list[0] = movie1;
-list[1] = movie2;
+      let color = (req.body.color) 
+      let colorResult = colorFunctions.color(color);
 
-res.render('results', {movies: list});
+      let rorschack = (req.body.rorschack)  
+      let rorschachResult = rorschachFunctions.rorschach(rorschack)
+
+
+    //console.log(weatherResult, color, rorschack)
+    let result = [weatherResult, colorResult, rorschachResult] 
+    console.log(weatherResult, colorResult, rorschachResult)
+      function getSum(x, y) { 
+            return x + y;
+      }
+    let sum = (result.reduce(getSum)) 
+      function average(x){ 
+        return Math.ceil(sum/x.length) //math.ceil always rounds up 
+      }
+    let Average = average(result)
+    let total = Average.toString()
+    
+    console.log(total)
+    let list = []
+    let movie1 = movies.database.randomMovie(total);
+    let movie2 = movies.database.randomMovie(total);
+    list[0] = movie1;
+    list[1] = movie2;
+
+    res.render('results', {movies: list});
 })
-  //res.redirect(response);
-//
-// app.post('/', (req, res) => {
-//   // //const emotion = req.body.emotion;
-//   // const colorInput 
-//   // // console.log(emotion);
-
- 
-// })
-/* WOULD BE NICE TO DO A WHEEL - SO WAITING 5 SECONDS.. 'WE ARE NOW GENERATING THE BEST MOVIE BASED ON YOUR CURRENT MOOD'*/
 
 app.get('/results', (req, res) => {
   res.render('results');
